e8aa2cf8c1c02eba291c08fc90e7e35ec96acc00
TURAN\turan <turan@suslu.eu>
2020-12-11 11:48:43 +0100

    De laatste bonus opdracht ik ben blij dat ik eindelijk klaar ben met deze opdracht  :D

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float delay = 0.9f;
+    private float tijd = 0;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
-
-        }
+            if (Input.GetKeyDown(KeyCode.Space) && tijd < Time.time)
+            {
+                tijd = Time.time + delay;
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
     }
 }
