3752b51bec81f8cab77ccc3f5a23eab645ebaefc
TURAN\turan <turan@suslu.eu>
2020-12-11 10:31:19 +0100

    Toegevoegd: extra text, Random  tijd spawner Wat er nog mis is de delay dog opdracht: ik weet niet hoe ik dat moet doen ik heb veel dingen geprobeert met Invoke

--- Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -18,7 +18,7 @@ public class DestroyOutOfBoundsX : MonoBehaviour
         // Destroy balls if y position is less than bottomLimit
         else if (transform.position.y < bottomLimit)
         {
-            Debug.Log("No one can escape the fate that was chosen for them.");
+            Debug.Log("No one can escape the fate that was chosen for them. All that remains is the end, where you will all perish. Eternal greatness exists only within myself. Sing a song of sorrow in a world where time has vanished.");
             Destroy(gameObject);
         }
 
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,11 +11,12 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnInterval;
 
     // Start is called before the first frame update
     void Start()
     {
+        spawnInterval = Random.Range(1.85f, 3.5f);
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
     }
 
